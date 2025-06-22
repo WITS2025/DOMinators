4ad690aeb9bccedd1d64e3c19f8a36d3c91bb8eb
Temima  Lewin <tlewin@wits.edu>
2025-06-22 15:50:57 -0400

    added abpit page

---
+++ About.jsx
@@ -0,0 +1,17 @@
+
+//import styles from '../styles/About.module.css';
+
+const About = () => {
+  return (
+    <div className={styles.container}>
+      <h2>About Plateful Search</h2>
+      <p>
+        Welcome to TripTrek! We want you to be able to plan your trip as seamlessly as possible.
+        Simply plan your itinerary and compare with others' iteneraries to plan the ultimate trip!
+        Happy travelling!
+      </p>
+    </div>
+  );
+};
+
+export default About;
