df842f3d536fa0dbe7ba9f9639a13bd8421d9207
Sara Nechama Isenberg <snisenberg@wits.edu>
2025-06-26 18:00:05 -0400

    fixing file locations, imports, and color theme styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,17 @@
-<<<<<<< trips-page-issue17
-import { useState } from 'react'
-import Trips from './pages/Trips'
-
-function App() {
-=======
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 // Import your page components
 import Home from './pages/Home';
-import About from './pages/AboutUs';
+import About from './pages/About';
 import ContactUs from './pages/ContactUs';
 import Trips from './pages/Trips';
 
 import NavigationBar from './components/NavigationBar';
 
->>>>>>> main
 
 function App() {
   return (
-<<<<<<< trips-page-issue17
-    <>
-
-    </>
-  )
-=======
     <Router>
       <div>
         <NavigationBar />
@@ -39,7 +26,6 @@ function App() {
       </div>
     </Router>
   );
->>>>>>> main
 }
 
 export default App;
