f89ec211ce7b0bd4ced0f31033e472e6b3bee09d
MiriamIny <miny@wits.edu>
2025-07-24 02:17:51 -0400

    added trip id route to router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,26 +5,29 @@ import Home from './pages/Home';
 import About from './pages/About';
 import ContactUs from './pages/ContactUs';
 import Trips from './pages/Trips';
-
+import TripDetail from './pages/TripDetail'; 
 import NavigationBar from './components/NavigationBar';
 
 
 function App() {
   return (
-    <Router>
-      <div>
-        <NavigationBar />
+    <>
+      <Router>
+        <div>
+          <NavigationBar />
 
-        {/* Route Definitions */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="/trips" element={<Trips />} />
-         
-        </Routes>
-      </div>
-    </Router>
+          {/* Route Definitions */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<ContactUs />} />
+            <Route path="/trips" element={<Trips />} />
+            <Route path="/trips/:tripId" element={<TripDetail />} />
+          
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
