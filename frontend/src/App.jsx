16d9a8f8b47c36d5d6f5779bbd29c33cf1876695
MiriamIny <miny@wits.edu>
2025-07-27 22:46:20 -0400

    fixed trip list render and import in app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,30 +4,28 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import ContactUs from './pages/ContactUs';
-import Trips from './pages/Trips';
+import TripList from './pages/TripList';
 import TripDetail from './pages/TripDetail'; 
 import NavigationBar from './components/NavigationBar';
 
 
 function App() {
   return (
-    <>
-      <Router>
-        <div>
-          <NavigationBar />
+    <Router>
+      <div>
+        <NavigationBar />
 
-          {/* Route Definitions */}
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<ContactUs />} />
-            <Route path="/trips" element={<Trips />} />
-            <Route path="/trips/:tripId" element={<TripDetail />} />
-          
-          </Routes>
-        </div>
-      </Router>
-    </>
+        {/* Route Definitions */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/trips" element={<TripList />} />
+          <Route path="/trips/:tripId" element={<TripDetail />} />
+        
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
