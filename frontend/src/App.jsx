83c68871d42396180508ae4cca8f93d249ae98e6
Chana Leah Nissel <clnissel@wits.edu>
2025-07-30 23:09:20 -0400

    created aws-exports.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,29 +3,28 @@ import NavigationBar from './components/NavigationBar';
 import Home from './pages/Home';
 import About from './pages/About';
 import ContactUs from './pages/ContactUs';
-import Trips from './pages/Trips';
+//import Trips from './pages/Trips';
 
 import TripList from './pages/TripList';
 import TripDetail from './pages/TripDetail'; 
-import NavigationBar from './components/NavigationBar';
+import { TripProvider } from './context/TripContext'; 
 
 function App() {
   return (
-    <Router>
-      <div>
-        <NavigationBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="/trips" element={<TripList />} />
-          <Route path="/trips/:tripId" element={<TripDetail />} />
-        </Routes>
-      </div>
-    </Router>
+    <TripProvider>
+      <Router>
+        <div>
+          <NavigationBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<ContactUs />} />
+            <Route path="/trips" element={<TripList />} />
+            <Route path="/trips/:tripId" element={<TripDetail />} />
+          </Routes>
+        </div>
+      </Router>
+    </TripProvider>
   );
 }
-
-export default App;
-
 
