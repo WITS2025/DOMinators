c771051b9d73c5b0ed2526afefed70e544a14eed
Chana Leah Nissel <clnissel@wits.edu>
2025-07-30 22:51:15 -0400

    resolved merge conflict with main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,11 @@ import NavigationBar from './components/NavigationBar';
 import Home from './pages/Home';
 import About from './pages/About';
 import ContactUs from './pages/ContactUs';
-<<<<<<< HEAD
 import Trips from './pages/Trips';
-=======
+
 import TripList from './pages/TripList';
 import TripDetail from './pages/TripDetail'; 
 import NavigationBar from './components/NavigationBar';
-
->>>>>>> origin/main
 
 function App() {
   return (
@@ -21,13 +18,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<ContactUs />} />
-<<<<<<< HEAD
-          <Route path="/trips" element={<Trips />} />
-=======
           <Route path="/trips" element={<TripList />} />
           <Route path="/trips/:tripId" element={<TripDetail />} />
-        
->>>>>>> origin/main
         </Routes>
       </div>
     </Router>
